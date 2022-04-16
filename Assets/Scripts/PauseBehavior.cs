4c5b6edf483da73eba6e3d84494c86b3ca38a52a
brianyyang <b.y.yang212@gmail.com>
2022-04-15 21:58:32 -0400

    Change pause to p

--- Assets/Scripts/PauseBehavior.cs
+++ Assets/Scripts/PauseBehavior.cs
@@ -10,7 +10,7 @@ public class PauseBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.P))
         {
             PauseOrUnpause();
         }
